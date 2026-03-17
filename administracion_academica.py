d62b4d5e922df75252619a2286d4980309409a72
GabrielBlancoDev <gtbvacation@outlook.es>
2026-03-16 21:23:01 -0500

    agregar funciones principales

--- administracion_academica.py
+++ administracion_academica.py
@@ -1,4 +1,4 @@
-nombre = input("Ingrese el nombre del estudiante: ")
+'''nombre = input("Ingrese el nombre del estudiante: ")
 
 nota=[]
 x=0
@@ -7,3 +7,11 @@ y=0
 estudiante[x]=input("Ingrese el nombre del estudiante: ")
 nota[y]=float(input("Ingrese la nota del estudiante: "))
 promedio[x]=(nota[y]+nota[y+1]+nota[y+2])/3
+'''
+
+def registrar_estudiante(nombre, edad, n1, n2, n3):
+    return nombre, edad, n1, n2, n3   
+
+def calcular_promedio(n1, n2, n3):
+    promedio = (n1 + n2 + n3) / 3
+    return promedio
